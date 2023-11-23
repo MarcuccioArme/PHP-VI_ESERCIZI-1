77261367b69927b3e48d772fff68207c54287bc8
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2023-11-23 15:29:04 +0100

    Modificato file

--- Eserc_4 Strutture di dati1/a.php
+++ Eserc_4 Strutture di dati1/a.php
@@ -1,23 +1,23 @@
 <?php
 
-    $vettore[] = 0;
+    $array = [];
 
     for ($i=0; $i<10; $i++) {
         $numero = rand(10,100);
-        $vettore[$i] = $numero;
+        $array[] = $numero;
     }
 
     echo "Vettore Base: <br>";
     for ($i=0; $i<10; $i++) {
-        $contenuto = $vettore[$i];
+        $contenuto = $array[$i];
         echo "$contenuto ";
     }
 
-    $vettoreReverse = array_reverse($vettore);
+    $arrayReverse = array_reverse($array);
 
     echo "<br><br>Vettore Reverse: <br>";
     for ($i=0; $i<10; $i++) {
-        $contenuto = $vettoreReverse[$i];
+        $contenuto = $arrayReverse[$i];
         echo "$contenuto ";
     }
 
